ea26ffb3a0225b726034d9b1ed67a8ba054e209b
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2021-12-10 23:59:41 -0800

    added image modal. - adjusted modal size

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
+    //video-modal
     // Set iframe attributes when the show instance method is called
     $("#video-modal").on("show.bs.modal", function(event) {
-        let link = $(event.relatedTarget); // link that triggered the modal
-        let url = link.attr('href');      // Extract url from href
+        let link = $(event.relatedTarget); // the link
+        let url = link.attr('href');      //the url
         $(this).find("iframe").attr({
             src : url,
             allow : "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         });
     });
 
-    // Remove iframe attributes when the modal has finished being hidden from the user
+    // stop video from playing when modal hidden
     $("#video-modal").on("hidden.bs.modal", function() {
         $("#video-modal iframe").removeAttr("src allow");
+    });
+});
+
+//img-modal
+$("#img-modal").on("show.bs.modal", function(event) {
+    let link = $(event.relatedTarget); // the link
+    let img = link.attr('href');      //the img src
+    $(this).find("img").attr({
+        src : img
     });
 });
